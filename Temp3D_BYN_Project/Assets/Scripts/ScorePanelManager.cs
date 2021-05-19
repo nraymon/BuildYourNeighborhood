d16503eedf8819d46ccf90bc572fc4b0fdea4be8
apriljames <jamesap@oregonstate.edu>
2021-05-19 00:51:28 -0700

    Score panel visibility can be toggled, panel is draggable
    The score panel can be toggled on with the "what caused my score" button, and closed with the "X" button in the panel. The panel can be dragged by clicking and dragging the panel banner. More to come later!

---
+++ Temp3D_BYN_Project/Assets/Scripts/ScorePanelManager.cs
@@ -0,0 +1,42 @@
+﻿/***************************************************
+Enables the score panel visibility to be toggled by buttons.
+***************************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ScorePanelManager : MonoBehaviour
+{
+    public GameObject ScorePanel;
+    public Button OpenScorePanelButton;
+    public Button CloseScorePanelButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        OpenScorePanelButton.onClick.AddListener(OpenScorePanel);
+        CloseScorePanelButton.onClick.AddListener(CloseScorePanel);
+    }
+
+
+    // function that turns on score panel visibility
+    public void OpenScorePanel()
+    {
+        if(ScorePanel != null)
+        {
+            ScorePanel.SetActive(true);
+        }
+    }
+
+    //function that turns off score panel visibility
+    public void CloseScorePanel()
+    {
+        if (ScorePanel != null)
+        {
+            ScorePanel.SetActive(false);
+        }
+    }
+}
