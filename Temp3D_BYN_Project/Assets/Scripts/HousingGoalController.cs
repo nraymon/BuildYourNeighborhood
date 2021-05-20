2fb35e77153af67fa2baa3814e49c0b0dbbff78e
vossli <43762980+vossli@users.noreply.github.com>
2021-05-20 13:10:28 -0700

    UI Screenshot

---
+++ Temp3D_BYN_Project/Assets/Scripts/HousingGoalController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class HousingGoalController : MonoBehaviour
+{
+    public Image bar;
+    public Text perc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bar.fillAmount -= .001f;
+        float j = (1 * bar.fillAmount);
+        string goal = j.ToString("n2");
+        goal = goal.Substring(2, 2);
+        perc.text = goal + "%";
+    }
+}
---
+++ Temp3D_BYN_Project/Assets/Scripts/UISpawn.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UISpawn : MonoBehaviour, IPointerDownHandler, IPointerExitHandler
+{
+
+    TileValues tVal;
+    GameObject temp;
+    StateManager state;
+
+    public TileValues.TileType tileType;
+
+    int num;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = GameObject.Find("GameManager").GetComponent<StateManager>();
+        tVal = GameObject.Find("GameManager").GetComponent<TileValues>();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (this.gameObject.GetComponent<UISpawn>().tileType == TileValues.TileType.wetlands)
+        {
+            //Vector3 mousePosFar = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.farClipPlane);
+            // Vector3 mousePosNear = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane);
+            Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Debug.Log(pos);
+            pos.x += 5f;
+            pos.y -= 2.5f;
+            pos.z += 5f;
+            Quaternion rot = Quaternion.identity;
+            //rot.z -= 1;
+            temp = Instantiate(tVal.GetWet(), pos, tVal.GetWet().transform.rotation);
+        }
+
+        if (this.gameObject.GetComponent<UISpawn>().tileType == TileValues.TileType.bioswale)
+        {
+            Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Debug.Log(pos);
+            pos.x += 5f;
+            pos.y -= 2.5f;
+            pos.z += 5f;
+            Quaternion rot = Quaternion.identity;
+
+            //rot.y += 1;
+            temp = Instantiate(tVal.GetBio(), pos, tVal.GetBio().transform.rotation);
+        }
+
+        if (this.gameObject.GetComponent<UISpawn>().tileType == TileValues.TileType.house)
+        {
+            Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Debug.Log(pos);
+            pos.x += 5f;
+            pos.y -= 2.5f;
+            pos.z += 5f;
+            Quaternion rot = Quaternion.identity;
+            //rot.y -= 1;
+            temp = Instantiate(tVal.GetHouse(), pos, tVal.GetHouse().transform.rotation);
+        }
+
+        if (this.gameObject.GetComponent<UISpawn>().tileType == TileValues.TileType.road)
+        {
+            Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Debug.Log(pos);
+            pos.x += 5.2f;
+            pos.y -= 2.8f;
+            pos.z += 5f;
+            Quaternion rot = Quaternion.identity;
+            //rot.z -= 1;
+            temp = Instantiate(tVal.GetRoad(), pos, tVal.GetRoad().transform.rotation);
+        }
+
+        temp.name = temp.name + num;
+        state.SetSpawn(false);
+
+        TileValues tileValues = GameObject.Find("GameManager").GetComponent<TileValues>();
+        tileValues.AssignValues(num, tileType);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        this.gameObject.SetActive(true);
+    }
+}
